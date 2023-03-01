36729191b9280695500e31e16a59c241431bf4c4
MYKYTA VOSTRIKOV <mykytavostrikov@gmail.com>
2023-03-01 20:28:49 +0200

    prepared app to deploy on vercel

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { products } from './productsRepository';
 import { app } from './settings'
 import { CodeResponsesEnum } from "./types/CodeResponsesEnum";
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.delete('/testing/all-data', (req: Request, res: Response) => {
   products.length = 0;;
